400a9fcd5e6e13c282aec56427c66a1d31d294dd
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2020-07-11 23:18:48 +0530

    Added code to upload, convert and download files

--- spring_example/src/main/java/com/pdfen/spring_example/SpringExampleApplication.java
+++ spring_example/src/main/java/com/pdfen/spring_example/SpringExampleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringExampleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleApplication.class, args);
+    }
 }
--- spring_example/src/main/java/com/pdfen/spring_example/models/CreateFileRequest.java
+++ spring_example/src/main/java/com/pdfen/spring_example/models/CreateFileRequest.java
@@ -1,9 +1,11 @@
 package com.pdfen.spring_example.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CreateFileRequest {
 
     @JsonProperty("file_settings")
